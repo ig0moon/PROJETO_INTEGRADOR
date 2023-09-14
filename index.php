c8d7a53c4f4a530baad292e869ddca48e026ddad
nosequeponeraquiXD <hernandez.7960@aluno.pr.senac.br>
2023-09-14 15:47:54 -0300

    https://www.youtube.com/watch?v=dQw4w9WgXcQ

--- index.php
+++ index.php
@@ -6,11 +6,19 @@
 <img src="img/OMAGAAA.jfif">
 <marquee>Omaagaa! centralizado</marquee>
 </div>
-<div>
+<div id="sonido">
 	<audio controls>
 		<source src="audio/OMGmeme.mp3" type="audio/mp3">
 	</audio>
 </div>
+<div id="link">
 <a href="https://www.youtube.com/watch?v=qUG9qNm4QzM" target="guia">&#128722;</a>
+</div>
+<div id="vid">
+	<p>Eu, Eu tentando centrar a div</p>
+	<video controls>
+		<source src="video/meme.mp4" type="video/mp4">
+	</video>
+</div>
 </body>
 </html>
