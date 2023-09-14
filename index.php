749012e4cfd3b9eddfbb830746a1654a8fe0ed36
ig0moon <igor.9946@aluno.pr.senac.br>
2023-09-14 15:16:08 -0300

    Omaga centralizado

--- index.php
+++ index.php
@@ -1,7 +1,11 @@
 <?php
 	require_once "cabecalho.php";
 ?>
+
+<div id="omaga">
 <img src="img/OMAGAAA.jfif">
-<marquee>Omaagaa!</marquee>
+<marquee>Omaagaa! centralizado</marquee>
+</div>
+
 </body>
 </html>
