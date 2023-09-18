47b51298289e424efa12bb052691195d5cbb1eeb
ig0moon <igor.9946@aluno.pr.senac.br>
2023-09-18 13:58:38 -0300

    merge

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+	require_once "cabecalho.php";
+?>
+
+
+
+	<audio controls>
+		<source src="audio/OMGmeme.mp3" type="audio/mp3">
+	</audio>
+</div>
+<div id="link">
+<a href="https://www.youtube.com/watch?v=qUG9qNm4QzM" target="guia">&#128722;</a>
+</div>
+<div id="vid">
+	<p>Eu, Eu tentando centrar a div</p>
+	<video controls>
+		<source src="video/meme.mp4" type="video/mp4">
+	</video>
+</div>
+</body>
+</html>
