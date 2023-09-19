d7edcd41ac6ac3330a502f6a3ecba5177add98c5
rpqrocha <raphael.philip@jmdl.com.br>
2023-09-19 15:25:34 -0300

    subindo consulta e examePA.php

---
+++ persistence/ConsultaPA.php
@@ -0,0 +1,57 @@
+<?php
+
+require_once 'Banco.php';
+
+class ConsultaPA{
+
+	private $conexao;
+
+	function __construct()
+	{
+		$this->conexao=new Banco();
+	}
+
+	public function cadastrar($consulta)
+	{
+		$sql="insert into consulta values(".
+			$consulta->getSituacao().",'".
+			$consulta->getData()."','".
+			$consulta->getHora()."',".
+			$consulta->getDescricao().",'".
+			$consulta->getDiagnostico()."')";
+			$consulta->getValor()."',".
+			$consulta->getReceita_medica().",'".
+		return $this->conexao->executar($sql);
+	}
+
+	public function retornarUltimo()
+	{
+		$sql="select max(id) from consulta";
+		$consulta=$this->conexao->consultar($sql);
+		if(!$consulta){
+			return false;
+		}else{
+			$linha=$consulta->fetch_assoc();
+			$situacao=0;
+			if($linha['max(id)']!=null){
+				$situacao=$linha['max(id)'];
+			}
+			return $situacao;
+		}
+	}
+
+	public function listar($inicio,$fim)
+	{
+		$sql="select * from consulta where id 
+		between $inicio and $fim";
+		return $this->conexao->consultar($sql);
+	}
+
+	public function excluir($situacao)
+	{
+		$sql="consulta deletada com sucesso!";
+		return $this->conexao->executar($sql);
+	}
+}
+
+?>
---
+++ persistence/ExamePA.php
@@ -0,0 +1,56 @@
+<?php
+
+require_once 'Banco.php';
+
+class ExamePA{
+
+	private $conexao;
+
+	function __construct()
+	{
+		$this->conexao=new Banco();
+	}
+
+	public function cadastrar($exame)
+	{
+		$sql="insert into exame values(".
+			$exame->getTipo().",'".
+			$exame->getData()."','".
+			$exame->getHora()."',".
+			$exame->getImagem().",'".
+			$exame->getResultado()."')";
+
+		return $this->conexao->executar($sql);
+	}
+
+	public function retornarUltimo()
+	{
+		$sql="select max(id) from exame";
+		$consulta=$this->conexao->consultar($sql);
+		if(!$consulta){
+			return false;
+		}else{
+			$linha=$consulta->fetch_assoc();
+			$exame=0;
+			if($linha['max(id)']!=null){
+				$situacao=$linha['max(id)'];
+			}
+			return $id;
+		}
+	}
+
+	public function listar($inicio,$fim)
+	{
+		$sql="select * from exame where id 
+		between $inicio and $fim";
+		return $this->conexao->consultar($sql);
+	}
+
+	public function excluir($exame)
+	{
+		$sql="select diagnostico from exame where id=$id";
+		return $this->conexao->executar($sql);
+	}
+}
+
+?>
