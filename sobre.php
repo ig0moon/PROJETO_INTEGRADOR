b052e1843fdf335f5b9e961cc6cb011a36d0758a
rpqrocha <raphael.philip@jmdl.com.br>
2023-09-20 16:46:59 -0300

    ajuda e sobre

---
+++ sobre.php
@@ -0,0 +1,51 @@
+<?php 
+
+require_once 'cabecalho.php'
+
+?>
+
+  <h1>Sobre:</h1>
+
+	<h2>Bem-vindo à OMAGA: O Sorriso que Transforma Vidas</h2>
+
+<p>Na Clínica Odontológica OMAGA, estamos comprometidos em transformar sorrisos e vidas. Nossa jornada começa com uma paixão profunda pela Odontologia e a missão de proporcionar cuidados de qualidade excepcional aos nossos pacientes.</p>
+
+	<h3>Nossa História</h3>
+
+<p>A história da Clínica OMAGA é uma história de dedicação, inovação e comprometimento com a saúde bucal. Fundada em 2023 pelos alunos do SENAC Ponta Grossa, integrantes de uma turma com renomados desenvolvedores com vasta experiência e conhecimento em diversas áreas da tecnologia. Em conjunto com estudantes do curso de odontologia da Universidade Estadual de Ponta Grossa, a OMAGA rapidamente se destacou no cenário odontológico devido à sua abordagem centrada no paciente.</p>
+
+<p>O nome "OMAGA" é uma junção das palavras "Odontologia" e "Maga", que significa "mágica" em latim, fazendo alusão à um meme da internet. Essa escolha reflete nosso compromisso em tornar a experiência odontológica tão suave, mágica e engraçada o máximo possível para nossos pacientes.</p>
+
+<p>Desde o início, nosso foco tem sido a excelência clínica e a satisfação do paciente. Investimos continuamente em tecnologia de ponta e na formação de nossa equipe, composta por profissionais altamente qualificados e apaixonados pelo que fazem. Acreditamos que a educação e a atualização constante são essenciais para oferecer tratamentos odontológicos de vanguarda.</p>
+
+	<h3>Nossa Missão</h3>
+
+<p>Na Clínica OMAGA, nossa missão é proporcionar cuidados odontológicos personalizados e de alta qualidade que transformem vidas. Acreditamos que um sorriso saudável é a chave para a confiança e o bem-estar geral. Trabalhamos em estreita colaboração com nossos pacientes para entender suas necessidades e objetivos individuais, criando planos de tratamento sob medida que atendam às suas expectativas.</p>
+
+	<h3>Nossos Serviços</h3>
+
+<p>Oferecemos uma ampla gama de serviços odontológicos, desde tratamentos preventivos até procedimentos avançados de reabilitação oral. Nossos serviços incluem:<p>
+
+	<li>
+		<ul>Limpeza e prevenção;</ul>
+		<ul>Tratamentos estéticos, como clareamento dental e facetas de porcelana;</ul>
+		<ul>Implantes dentários e reabilitação oral;</ul>
+		<ul>Ortodontia, incluindo Invisalign®;</ul>
+		<ul>Cirurgia bucal e maxilofacial;</ul>
+		<ul>Odontopediatria para cuidar dos sorrisos das crianças;</ul>
+		<ul>Tratamento de periodontite e gengivite e;</ul>
+		<ul>Restaurações de alta qualidade, incluindo restaurações estéticas e coroas.</ul>
+	</li>
+
+	<h3>Nossa Dedicação à Comunidade</h3>
+
+<p>Na OMAGA, não apenas cuidamos dos sorrisos de nossos pacientes, mas também estamos comprometidos com a comunidade. Realizamos programas de conscientização odontológica em escolas locais e participamos de iniciativas de caridade para oferecer tratamento odontológico a pessoas necessitadas.</p>
+
+<p>Em cada consulta, nossa equipe se esforça para criar um ambiente acolhedor e amigável, onde nossos pacientes se sintam à vontade. Valorizamos a comunicação aberta e transparente, colocando o paciente no centro de todas as decisões relacionadas ao tratamento.</p>
+
+	<h3>Venha Transformar Seu Sorriso Conosco!</h3>
+
+<p>Na Clínica Odontológica OMAGA, acreditamos que um sorriso saudável é uma porta de entrada para uma vida mais feliz e confiante. Estamos aqui para ajudá-lo a alcançar o sorriso dos seus sonhos. Marque uma consulta hoje mesmo e descubra como podemos transformar sua saúde bucal e sua qualidade de vida. Junte-se a nós nesta jornada para um sorriso que transforma vidas!</p>
+
+</body>
+</html>
