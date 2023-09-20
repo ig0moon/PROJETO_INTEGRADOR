596cdaaaf6a49a112c5c7da38739401c8b1db1eb
rpqrocha <raphael.philip@jmdl.com.br>
2023-09-20 17:29:12 -0300

    AJUDA E SOBRE ATUALIZADO 2

--- ajuda.php
+++ ajuda.php
@@ -5,47 +5,47 @@ require_once 'cabecalho.php'
 ?>
 
   		<h1>Perguntas Frequentes:</h1>
-
-
+<br>
+<br>
 	<h2>Quais são os serviços oferecidos pela sua clínica odontológica?</h2>
-
+<br>
 <p>Oferecemos uma ampla gama de serviços odontológicos, incluindo limpeza dental, restaurações, tratamento de canal, extrações, implantes, ortodontia, odontopediatria e muito mais. Nossa equipe está preparada para atender às suas necessidades odontológicas.</p>
-
+<br>
 	<h2>Quando devo marcar minha próxima consulta de rotina?</h2>
-
+<br>
 <p>Recomendamos que os pacientes agendem uma consulta de rotina a cada seis meses. No entanto, isso pode variar de acordo com as necessidades individuais. Se você tiver alguma dúvida sobre a frequência das consultas, consulte o seu dentista.</p>
-
+<br>
 	<h2>Quais são os sinais de emergência odontológica?</h2>
-
+<br>
 <p>Os sinais de emergência odontológica podem incluir dor intensa, sangramento persistente, inchaço facial grave, um dente quebrado ou um dente permanentemente perdido. Se você experimentar algum desses sintomas, entre em contato conosco imediatamente.</p>
-
+<br>
 	<h2>Como faço para marcar uma consulta?</h2>
-
+<br>
 <p>Você pode marcar uma consulta ligando para o nosso consultório durante o horário comercial ou utilizando nosso sistema de agendamento online em nosso site.</p>
-
+<br>
 	<h2>Qual é a política de cancelamento de consultas?</h2>
-
+<br>
 <p>Pedimos que você nos avise com pelo menos 24 horas de antecedência caso precise cancelar ou remarcar sua consulta. Isso nos ajuda a atender outros pacientes de forma eficaz.</p>
-
+<br>
 	<h2>Quais são as opções de pagamento aceitas?</h2>
-
+<br>
 <p>Aceitamos pagamentos em dinheiro, cheque, cartões de crédito e débito. Também oferecemos opções de financiamento para tratamentos mais extensos.</p>
-
+<br>
 	<h2>Como posso cuidar melhor da minha saúde bucal em casa?</h2>
-
+<br>
 <p>Além de visitas regulares ao dentista, é importante escovar os dentes pelo menos duas vezes ao dia, usar fio dental diariamente, evitar alimentos ricos em açúcar e praticar uma boa higiene bucal em casa. Se tiver dúvidas sobre sua rotina de cuidados bucais, pergunte ao seu dentista durante a consulta.</p>
-
+<br>
 	<h2>Vocês atendem pacientes de todas as idades?</h2>
-
+<br>
 <p>Sim, atendemos pacientes de todas as idades, desde crianças até idosos. Nossa equipe está qualificada para oferecer tratamento odontológico personalizado para todas as faixas etárias.</p>
-
+<br>
 	<h2>Qual é a diferença entre um dentista geral e um especialista?</h2>
-
+<br>
 <p>Um dentista geral realiza uma variedade de tratamentos odontológicos básicos, enquanto um especialista possui treinamento adicional em áreas específicas, como ortodontia, periodontia ou endodontia. Se necessário, encaminharemos você a um especialista qualificado para cuidados mais específicos.</p>
-
+<br>
 	<h2>Vocês aceitam planos de seguro odontológico?</p>
-
+<br>
 <p>Sim, aceitamos muitos planos de seguro odontológico. Entre em contato conosco com informações sobre seu seguro, e nossa equipe terá prazer em ajudá-lo a entender os detalhes da cobertura.</p>
-
+<br>
 </body>
 </html>
