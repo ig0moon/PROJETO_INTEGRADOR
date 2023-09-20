8214aeae514ad25dd0823368066877c03e15c8c7
leonardo200313 <leleuiurko@gmail.com>
2023-09-20 14:53:27 -0300

    listar dentista

---
+++ listardentista.php
@@ -0,0 +1,39 @@
+<?php
+require_once 'cabecalho.php';
+
+require_once 'persistence/DentistaPA.php';
+$dentistapa=new DentistaPA();
+$consulta=$dentistapa->listar();
+if(!$consulta){
+	echo "<h2>Ainda estamos cadastrando!</h2>";
+}else{
+
+	echo "<table>";
+	echo "<tr>";
+	echo "<th>Especialidade</th>";
+	echo "<th>Nome</th>";
+	echo "<th>Endereço</th>";
+	echo "<th>Telefone</th>";
+	echo "<th>Email</th>";
+	echo "<th>CRM</th>";
+	echo "</tr>";
+
+	while($linha=$consulta->fetch_assoc()){
+		echo "<tr>";
+		echo "<td>".$linha['especialidade']."</td>";
+		echo "<td>".$linha['nome']."</td>";
+		echo "<td>".$linha['endereco']."</td>";
+		echo "<td>".$linha['telefone']."</td>";
+		echo "<td>".$linha['email']."</td>";
+		echo "<td>".$linha['crm']."</td>";
+		
+		echo "</tr>";
+	}
+	echo "</table>";
+
+	echo "<section>";
+	
+}
+?>
+</body>
+</html>
