f87f1ba170239a074607d8010bafcf7ec748602c
ig0moon <riders.igo407@gmail.com>
2023-09-20 17:26:22 -0300

    MENU ADMIN COM TARGET

--- principal.php
+++ principal.php
@@ -18,14 +18,14 @@
 <?php
     if (isset($_COOKIE['admin'])) {
         // echo "<li><a href='/PROJETO_INTEGRADOR/admin'>Admin</a></li>";
-        echo "<li><a href='/PROJETO_INTEGRADOR/cadden'>Cadastrar Dentista</a></li>";
-        echo "<li><a href='/PROJETO_INTEGRADOR/listarden'>Listar Dentista</a></li>";
-        echo "<li><a href='/PROJETO_INTEGRADOR/cadpac'>Cadastrar Paciente</a></li>";
-        echo "<li><a href='/PROJETO_INTEGRADOR/listarpac'>Listar Paciente</a></li>";
-        echo "<li><a href='/PROJETO_INTEGRADOR/cadcon'>Cadastrar Consulta</a></li>";
-        echo "<li><a href='/PROJETO_INTEGRADOR/listarcon'>Listar Consulta</a></li>";
-        echo "<li><a href='/PROJETO_INTEGRADOR/cadex'>Cadastrar Exame</a></li>";
-        echo "<li><a href='/PROJETO_INTEGRADOR/listarex'>Listar Exame</a></li>";
+        echo "<li><a href='/PROJETO_INTEGRADOR/cadden' target='quadro'>Cadastrar Dentista</a></li>";
+        echo "<li><a href='/PROJETO_INTEGRADOR/listarden' target='quadro'>Listar Dentista</a></li>";
+        echo "<li><a href='/PROJETO_INTEGRADOR/cadpac' target='quadro'>Cadastrar Paciente</a></li>";
+        echo "<li><a href='/PROJETO_INTEGRADOR/listarpac' target='quadro'>Listar Paciente</a></li>";
+        echo "<li><a href='/PROJETO_INTEGRADOR/cadcon' target='quadro'>Cadastrar Consulta</a></li>";
+        echo "<li><a href='/PROJETO_INTEGRADOR/listarcon' target='quadro'>Listar Consulta</a></li>";
+        echo "<li><a href='/PROJETO_INTEGRADOR/cadex' target='quadro'>Cadastrar Exame</a></li>";
+        echo "<li><a href='/PROJETO_INTEGRADOR/listarex' target='quadro'>Listar Exame</a></li>";
         echo "<li><a href='/PROJETO_INTEGRADOR/sairadmin'>Sair</a></li>";
 
     } else if (isset($_COOKIE['cliente'])) {
