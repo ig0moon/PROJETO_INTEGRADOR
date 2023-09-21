3bb6b8d747f3153f38246160f834afd8ff050182
nosequeponeraquiXD <140538914+nosequeponeraquiXD@users.noreply.github.com>
2023-09-21 20:54:52 -0300

    acho que ja esta
    PD: si esta errado e culpa do igor, PD2: se estiver errado eu choro pipipi

--- listarresultados.php
+++ listarresultados.php
@@ -1,17 +1,47 @@
-<?php  
-require_once 'cabecalho.php';
-require_once 'model/Exame.php';
+<?php
+	require_once 'cabecalho.php';
 
-if (isset($_COOKIE['resultado'])) {
-	$exame=new Exame();
-	$consulta=$exame->setResultado($resultado);
-	echo "<h2>".$consulta->getResultado()."</h2>";
-	$consulta=$exame->setResultado($resultado);
-}else{
-	if (!$consulta) {
-		echo "<h2>gei</h2>";
+	if(isset($_POST['inicio'])){
+		$inicio=$_POST['inicio'];
+		$fim=$inicio+4;
+
+	} else{
+		$inicio=1;
+		$fim=5;
 	}
-}
 
+	require_once 'persistence/ExamePA.php';
+	$examepa=new ExamePA();
+	$resultados=$examepa->listarResultado($inicio,$fim);
+	if (!$resultados){
+		echo "<h2>Ainda não tem resultados.</h2>";
 
+	}else{
+		echo "<table>";
+		echo "<tr>";
+		echo "<th>Id</th>";
+		echo "<th>Resultado</th>";
+		echo "</tr>";
+
+		while ($linha=$resultados->fetch_assoc()){
+			echo "<tr>";
+			echo "<td>".$linha['id_examen_pk']."</td>";
+			echo "<td>".$linha['resultado']."</td>";
+			echo "</tr>";
+		}
+		echo "</table>";
+
+	echo "<section>";
+    $max=$examepa->retornarUltimo();
+    if ($fim<$max){
+        $inicio=$fim+1;
+        echo "<form action='listarresultados.php' method='POST'>";
+        echo "<input type='hidden' name='inicio'
+         value='$inicio'>";
+          echo "</form>";
+        }
+        echo "</section>";
+ }
 ?>
+</body>
+
