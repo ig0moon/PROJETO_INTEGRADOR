602c54c7f1171d11911ca5931298b5a0f1b6ef20
ig0moon <riders.igo407@gmail.com>
2023-09-21 15:42:54 -0300

    TAMANHO DA FONTE BLOG

--- logaradmin.php
+++ logaradmin.php
@@ -1,16 +1,21 @@
 <?php
 require_once 'cabecalho.php';
 ?>
-<form action="" class="normal" method="POST">
-	<h2>Login Administrador</h2>
-	<p>Usuário:<input type="text" name="admin" 
-		size="20" maxlength="20" 
-		pattern="[0-9a-zA-Z_]{1,20}" required></p>
-	<p>Senha:<input type="password" name="senha" 
-		size="10" maxlength="10" 
-		pattern="[0-9a-zA-Z_\s@]{1,10}" required></p>
-	<p><input type="submit" name="botao" value="Logar"></p>
-</form>
+
+<div id="painel">
+	<form action="" class="normal" method="POST">
+		<h2>Login Administrador</h2>
+
+		<p>Usuário:</p>
+			<p><input type="text" name="admin" size="20" maxlength="20" pattern="[0-9a-zA-Z_]{1,20}" required></p>
+
+		<p>Senha:</p>
+			<p><input type="password" name="senha" size="10" maxlength="10" pattern="[0-9a-zA-Z_\s@]{1,10}" required></p>
+
+		<p><input class="btn" type="submit" name="botao" value="Logar"></p>
+	</form>
+</div>
+
 <?php
 	if (isset($_POST['botao'])) {
 		require_once 'model/Admin.php';
@@ -32,8 +37,6 @@ require_once 'cabecalho.php';
 		}
 
 	}
-
-
 ?>
 </body>
 </html>
